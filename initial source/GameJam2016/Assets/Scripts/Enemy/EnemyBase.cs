fd2b6d56551a26408e1f0c83d754f637ff74085a
SirVoxelot <eugene.flormata@gmail.com>
2016-01-30 04:28:54 -0800

    Adding EnemyBase and EnemyHead

---
+++ initial source/GameJam2016/Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum EnemyBaseState { IDLE, PATROL, OFFENSE, DEFENSE }
+
+public class EnemyBase : MonoBehaviour
+{
+    
+    public EnemyBaseState CurrentState = EnemyBaseState.IDLE;
+    public GameObject WaypointCollection;
+    public float WaypointThreshold = 0.5f;
+
+    private NavMeshAgent agent;
+    private EnemyHead head;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        head = GetComponent<EnemyHead>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (CurrentState)
+        {
+            case EnemyBaseState.IDLE:
+                break;
+
+            case EnemyBaseState.PATROL:
+                this.updatePatrol();
+                if (head != null)
+                {
+                    head.LookForPlayer();
+                }
+                break;
+
+            case EnemyBaseState.OFFENSE:
+                break;
+
+            case EnemyBaseState.DEFENSE:
+                break;
+
+            default:
+                throw new System.Exception("Current State is not set");
+        }
+    }
+
+    private void updatePatrol()
+    {
+        float distanceRemaining = Vector3.Magnitude(agent.destination - this.transform.position) - agent.radius;
+
+        if (distanceRemaining <= WaypointThreshold)
+        {
+            agent.destination = this.findRandomTargetPosition();
+        }
+
+        Component body = this.GetComponentInChildren<BoxCollider>();
+        Debug.DrawLine(body.transform.position, agent.destination, new Color(0.75f, 1.0f, 0.5f, 1.0f));
+        Debug.DrawRay(body.transform.position, this.transform.forward * 100, new Color(0.75f, 0.5f, 1.0f, 1.0f));
+    }
+
+    private Vector3 findRandomTargetPosition()
+    {
+
+        if (WaypointCollection == null)
+            throw new System.Exception("No waypoint collection provided");
+
+        List<Transform> waypointList = new List<Transform>();
+        foreach (Transform child in WaypointCollection.transform.GetComponentsInChildren<Transform>())
+        {
+            if (child.tag.Equals("Waypoint") && !child.transform.position.Equals(agent.destination))
+            {
+                waypointList.Add(child);
+            }
+        }
+        
+        if (waypointList.Count == 0)
+            throw new System.Exception("No waypoints found in waypoint collection");
+
+        int randomIndex = Random.Range(0, waypointList.Count);
+
+        return waypointList[randomIndex].position;
+    }
+}
---
+++ initial source/GameJam2016/Assets/Scripts/Enemy/EnemyHead.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum EnemyHeadState { SEARCHING, LOCKED }
+
+public class EnemyHead : MonoBehaviour
+{
+    public float RotationPeriod = 2.0f;
+    public float RotationLimitX = 75.0f;
+    public float RotationLimitY = 10.0f;
+
+    private Transform head;
+    private Transform body;
+    private EnemyHeadState currentState;
+    private Transform lookAtTarget;
+    private float searchStartTime = 0.0f;
+    private bool searchingLeft = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        head = this.transform.FindChild("Head");
+        body = this.transform.FindChild("Body");
+        if (head == null)
+            throw new System.Exception("Unable to find Head");
+        if (body == null)
+            throw new System.Exception("Unable to find Body");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch (currentState)
+        {
+            case EnemyHeadState.SEARCHING:
+                this.updateSearching();
+                break;
+
+            case EnemyHeadState.LOCKED:
+                this.updateLocked();
+                break;
+        }
+    }
+
+    private void updateSearching()
+    {
+        if (this.searchStartTime <= 0.0f) //Initialize the value if not set
+            this.searchStartTime = Time.time;
+
+        float deltaTime = Time.time - this.searchStartTime;
+
+        Quaternion lookingLeft = Quaternion.AngleAxis(-RotationLimitX, Vector3.up); //Looking left
+        Quaternion lookingRight = Quaternion.AngleAxis(RotationLimitX, Vector3.up); //Looking right
+
+        float halfPeriod = this.RotationPeriod / 2;
+
+        float remaining = 0f;
+        if (this.searchingLeft == true)
+        {
+            head.localRotation = Quaternion.Lerp(lookingRight, lookingLeft, (deltaTime % halfPeriod) / halfPeriod);
+            remaining = Quaternion.Angle(head.localRotation, lookingLeft);
+        }
+        else
+        {
+            head.localRotation = Quaternion.Lerp(lookingLeft, lookingRight, (deltaTime % halfPeriod) / halfPeriod);
+            remaining = Quaternion.Angle(head.localRotation, lookingRight);
+        }
+
+        if (remaining <= 5.0f)
+        {
+            this.searchStartTime = Time.time;
+            this.searchingLeft = !this.searchingLeft;
+        }
+
+        Debug.DrawRay(head.position, head.forward * 200, Color.red);
+    }
+
+    private void updateLocked()
+    {
+        if (lookAtTarget == null)
+            return;
+
+        head.LookAt(lookAtTarget);
+    }
+
+    public void LookForPlayer()
+    {
+        if (currentState != EnemyHeadState.SEARCHING)
+        {
+            this.searchStartTime = Time.time;
+            currentState = EnemyHeadState.SEARCHING;
+        }
+    }
+
+    public void LookAt(Transform target)
+    {
+        lookAtTarget = target;
+        currentState = EnemyHeadState.LOCKED;
+    }
+}
