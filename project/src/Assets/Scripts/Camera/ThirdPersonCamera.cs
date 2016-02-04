e86dd5eefc1c566db675f3d199ccba70d6765a29
foozballz <foozballz5@yahoo.com>
2016-02-03 22:17:25 -0800

    Adding multi display support

---
+++ project/src/Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public Transform lookAtTarget;
+    public float distance = 10.0f;
+    public float angleX = 45.0f;
+    public float angleY = 0.0f;
+    public float angleZ = 0.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (lookAtTarget != null)
+        {
+            Vector3 cameraDirection = Quaternion.Euler(0f, lookAtTarget.rotation.eulerAngles.y, 0f) * Quaternion.Euler(angleX, angleY, angleZ) * Vector3.forward;
+            this.transform.position = lookAtTarget.transform.position + (cameraDirection * -distance);
+            this.transform.LookAt(lookAtTarget);
+        }
+    }
+}
--- project/src/Assets/Scripts/GameControl.cs
+++ project/src/Assets/Scripts/GameControl.cs
@@ -15,8 +15,10 @@ public class GameControl : MonoBehaviour {
     public static GameControl control;
     public GameObject level;
 
+    private bool enableMultiDisplay = false;
+    private int multiDisplayCount = 1;
 
-	// Use this for initialization
+    // Use this for initialization
     void Awake()
     {
         if (control == null)
@@ -28,9 +30,10 @@ public class GameControl : MonoBehaviour {
         {
             Destroy(gameObject);
         }
+
+        activateMultiDisplays();
     }
 	void Start () {
-
         //init
         level = null;
         sceneName = portalName = "";
@@ -39,6 +42,8 @@ public class GameControl : MonoBehaviour {
         dash = false;
         shoot = false;
         Load();
+
+        createMultiDisplayCameras();
 	}
 	
 	// Update is called once per frame
@@ -88,6 +93,41 @@ public class GameControl : MonoBehaviour {
         bf.Serialize(file, data);
         file.Close();
 
+    }
+
+    private void activateMultiDisplays()
+    {
+        string[] args = System.Environment.GetCommandLineArgs();
+
+        foreach (string arg in args)
+        {
+            switch (arg)
+            {
+                case "-multidisplay":
+                    enableMultiDisplay = true;
+                    break;
+            }
+        }
+
+        if (enableMultiDisplay && Display.displays.Length > 1)
+        {
+            Display.displays[1].Activate();
+            multiDisplayCount++;
+        }
+    }
+
+    private void createMultiDisplayCameras()
+    {
+        if (enableMultiDisplay && multiDisplayCount > 1)
+        {
+            UnityEngine.Object cameraObject = Resources.Load("Camera/ThirdPersonCamera", typeof(GameObject));
+            GameObject camera = GameObject.Instantiate(cameraObject, Vector3.zero, Quaternion.identity) as GameObject;
+            camera.name = "Camera Display 2";
+            camera.GetComponent<Camera>().targetDisplay = 1;
+
+            GameObject hero = GameObject.Find("asset_hero");
+            camera.GetComponent<ThirdPersonCamera>().lookAtTarget = hero.transform;
+        }
     }
 }
 
